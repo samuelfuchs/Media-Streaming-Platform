dce83e0ac00112de95a45c139e5eacb8596559bd
samuelfuchs <fuchsgerson@gmail.com>
2022-05-28 10:47:33 -0300

    wip working on implementing title and big background image

--- src/Banner.js
+++ src/Banner.js
@@ -1,19 +1,41 @@
 import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import requests from "./requests";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
+
   useEffect(() => {
     async function fetchData() {
-      //
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
     }
     fetchData();
   }, []);
+
+  console.log(movie);
+
   return (
-    <header>
-      {/* <<< bgimgage */}
-      {/* title */}
-      {/* div > 2 buttons */}
-      {/* description */}
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+            "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+        )`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        {/* title */}
+        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        {/* div > 2 buttons */}
+        {/* description */}
+      </div>
     </header>
   );
 }
